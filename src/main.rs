e978df2a5bfb1c2f43462fbed81e4252377a731b
Chris West (Faux) <git@goeswhere.com>
2020-01-01 22:58:03 +0000

    feat: graceful shutdown on signal or error

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+use std::cell::Cell;
 use std::collections::HashMap;
 use std::convert::Infallible;
 use std::net::SocketAddr;
+use std::sync::Arc;
 
 use failure::Error;
 use hyper::service::make_service_fn;
@@ -10,6 +12,7 @@ use hyper::Request;
 use hyper::Response;
 use hyper::Server;
 use swisher::reqs::SimpleMethod;
+use tokio::sync::mpsc;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
@@ -17,21 +20,52 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
 
     let addr = SocketAddr::from(([0, 0, 0, 0], 8202));
 
-    let make_svc =
-        make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(catch_handler)) });
+    let (shutdown, mut is_shutdown) = mpsc::channel::<()>(1);
+
+    let on_signal = Cell::new(Some(shutdown.clone()));
+    ctrlc::set_handler(move || {
+        let on_signal = on_signal.take();
+        match on_signal {
+            Some(mut on_signal) => {
+                log::warn!(
+                    "signal, attempting shutdown, status: {:?}",
+                    on_signal.try_send(()).is_err()
+                );
+            }
+            None => log::error!("ignoring termination signal"),
+        }
+    })?;
+
+    let make_svc = make_service_fn(move |_conn| {
+        let shutdown = shutdown.clone();
+        async move { Ok::<_, Infallible>(service_fn(move |req| catch_handler(req, shutdown.clone()))) }
+    });
 
     log::info!("server starting on http://localhost:8202/");
 
-    Server::bind(&addr).serve(make_svc).await?;
-    Ok(())
+    Server::bind(&addr)
+        .serve(make_svc)
+        .with_graceful_shutdown(async {
+            let _ = is_shutdown.recv().await;
+        })
+        .await?;
+
+    Err("server exited".into())
 }
 
-async fn catch_handler(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+async fn catch_handler(
+    req: Request<Body>,
+    mut shutdown: mpsc::Sender<()>,
+) -> Result<Response<Body>, Infallible> {
     // TODO: was expecting to catch_panic here but hyper doesn't want to play
     Ok(match handler(req).await {
         Ok(response) => response,
         Err(e) => {
             log::error!("internal error: {:?}", e);
+            log::warn!(
+                "error, attempting shutdown, status: {:?}",
+                shutdown.try_send(()).is_err()
+            );
             Response::builder()
                 .status(500)
                 .body(Body::empty())
